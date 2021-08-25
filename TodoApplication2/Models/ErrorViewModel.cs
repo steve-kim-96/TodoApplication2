a1a706ca6d67ce15db438f43f44fb24ad7d949cc
Soungku Kim <soungku@hotmail.com>
2021-08-25 19:10:42 +1200

    Add project files.

---
+++ TodoApplication2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TodoApplication2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
