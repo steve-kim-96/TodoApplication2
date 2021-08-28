1e246076977105b015ffa8706d7460b9dcf46f4c
steve-kim-96 <soungku@hotmail.com>
2021-08-28 17:18:40 +1200

    update dependencies and connections for data model

---
+++ TodoApplication2/Data/TodoApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TodoApplication2.Models;
+
+namespace TodoApplication2.Data
+{
+    public class TodoApplicationContext : DbContext
+    {
+        public TodoApplicationContext (DbContextOptions<TodoApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoApplication2.Models.Todo> Todo { get; set; }
+    }
+}
---
+++ TodoApplication2/Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApplication2.Models
+{
+    public class Todo
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DoneByDate { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
