811b5a6398fb709b5a10a2318f38c7ac4877cfb4
steve-kim-96 <soungku@hotmail.com>
2021-08-28 17:18:03 +1200

    test controller connection to views

---
+++ TodoApplication2/Controllers/TodoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace TodoApplication2.Controllers
+{
+    public class TodoController : Controller
+    {
+        // GET: /Todo
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Todo/Welcome
+        // By default the parameters are received from the URL by the model binder
+        // use [FromBody/Header/...] to change default
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
