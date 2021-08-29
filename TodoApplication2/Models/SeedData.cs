8609f1de0bf1e8b6c40bffb80f8adccf6b30e05a
steve-kim-96 <soungku@hotmail.com>
2021-08-29 13:27:14 +1200

    add initial seed data

---
+++ TodoApplication2/Models/SeedData.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApplication2.Data;
+
+namespace TodoApplication2.Models
+{
+    public class SeedData
+    {
+        public static object DateTimeTime { get; private set; }
+
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new TodoApplicationContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<TodoApplicationContext>>()))
+            {
+                if (context.Todo.Any())
+                {
+                    return;
+                }
+
+                context.Todo.AddRange(
+                    new Todo
+                    {
+                        Title = "Learn about context",
+                        DoneByDate = DateTime.Parse("30-08-21"),
+                        IsCompleted = false
+                    },
+
+                    new Todo
+                    {
+                        Title = "Learn about Database providers",
+                        DoneByDate = DateTime.Parse("28-09-21"),
+                        IsCompleted = false
+                    },
+
+                    new Todo
+                    {
+                        Title = "Learn more about C#",
+                        DoneByDate = DateTime.Parse("30-12-21"),
+                        IsCompleted = false
+                    },
+
+                    new Todo
+                    {
+                        Title = "Learn about Models",
+                        DoneByDate = DateTime.Parse("28-09-21"),
+                        IsCompleted = false
+                    },
+
+                    new Todo
+                    {
+                        Title = "Learn about Controllers",
+                        DoneByDate = DateTime.Parse("28-09-21"),
+                        IsCompleted = false
+                    }
+                   );
+                context.SaveChanges();
+            }
+        }
+    }
+}
