f43dfbdc80ed2a827da247c6be50a9cbeab54393
steve-kim-96 <soungku@hotmail.com>
2021-09-04 18:44:54 +1200

    add data annotations for max text

--- TodoApplication2/Models/Todo.cs
+++ TodoApplication2/Models/Todo.cs
@@ -9,9 +9,15 @@ namespace TodoApplication2.Models
     public class Todo
     {
         public int ID { get; set; }
+        [MaxLength(50)]
         public string Title { get; set; }
+        [Display(Name = "Type of chore")]
+        [MaxLength(50)]
+        public string? Type { get; set; }
+        [Display(Name="Done By Date")]
         [DataType(DataType.Date)]
         public DateTime DoneByDate { get; set; }
+        [Display(Name = "Is Completed")]
         public bool IsCompleted { get; set; }
     }
 }
