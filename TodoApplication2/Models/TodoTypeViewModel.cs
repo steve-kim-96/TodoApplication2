de614585512d90bae6b5571670d5d339f7cb62f3
steve-kim-96 <soungku@hotmail.com>
2021-09-05 14:53:33 +1200

    create and implement view model

---
+++ TodoApplication2/Models/TodoTypeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApplication2.Models
+{
+    public class TodoTypeViewModel
+    {
+        public List<Todo> Todos { get; set; }
+        public SelectList Types { get; set; }
+        public string TodoType { get; set; }
+        public string SearchString { get; set; }
+    }
+}
